e2b641f6236ee2fb75be5fa371095d3a95432279
Anil Saini <theaksaini@gmail.com>
2024-11-14 15:38:23 -0800

    modified datautils

--- datautils.py
+++ datautils.py
@@ -8,6 +8,7 @@ def generate_training_test_data(data_dir, dataset_name, rand_seed):
     '''
     Read from two csv files corresponding to a dataset containg 'edge cases' and 'random cases'.
     Create dataframes X_train and Y_train of size 200 that includes all egde cases and, if neddeed, rest from random cases dataset. 
+    X_val and Y_val contain 200 cases from random cases.
     X_test and Y_test contain 2000 cases from random cases.
 
     Parameters:
@@ -16,11 +17,13 @@ def generate_training_test_data(data_dir, dataset_name, rand_seed):
     rand_seed: int: random seed for reproducibility
 
     Returns:
-    X_train, y_train, X_test, y_test: DataFrames: training and testing data
+    X_train, y_train, X_val, y_val, X_test, y_test: DataFrames: training and testing data
     '''
 
     edge_case = pd.read_csv(f"{data_dir}/{dataset_name}/{dataset_name}-edge.csv")
     random_cases = pd.read_csv(f"{data_dir}/{dataset_name}/{dataset_name}-random.csv")
+
+    assert len(edge_case) <= 200, "The code assumes that the edge cases file contains at most 200 cases."
 
     # Ensure we have 200 training cases
     train = pd.concat([edge_case, random_cases.sample(n=200 - len(edge_case), random_state=rand_seed)])
@@ -28,14 +31,20 @@ def generate_training_test_data(data_dir, dataset_name, rand_seed):
     X_train = train[input_cols]
     y_train = train.drop(columns=input_cols)
 
+    # Ensure we have 200 validation and 2000 test cases
+    val_test = random_cases.sample(n=2200, random_state=rand_seed)
+    val = val_test.iloc[:200]
+    test = val_test.iloc[200:]
+    
+    input_cols = [col for col in val.columns if col.startswith("input")]
+    X_val = val[input_cols]
+    y_val = val.drop(columns=input_cols)
 
-    # Ensure we have 2000 test cases
-    test = random_cases.sample(n=2000, random_state=rand_seed)
     input_cols = [col for col in test.columns if col.startswith("input")]
     X_test = test[input_cols]
     y_test = test.drop(columns=input_cols)
    
-    return X_train, y_train, X_test, y_test
+    return X_train, y_train, X_val, y_val, X_test, y_test
 
 
 def get_problem_metadata(metadata_file, problem):
